c589cd5aee944fbf383a085b1d4a57a3417511cd
MakenRosa <maken.cristhian@gmail.com>
2023-02-22 17:06:36 -0300

    CRUD semi-completo

--- projeto-todo/todo/tasks/urls.py
+++ projeto-todo/todo/tasks/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloworld/', views.helloWorld),
     path('', views.taskList, name='task-list'),
-    path('yourname/<str:name>/', views.yourName, name='your-name')
+    path('task/<int:id>/', views.taskView, name='task-view'),
+    path('newtask/', views.newTask, name='new-task'),
+    path('edittask/<int:id>/', views.editTask, name='edit-task'),
+    path('delete/<int:id>/', views.deleteTask, name='delete-task')
 ]
