f89dad49d64793bc7716d80552f8f2ddbcaedbf9
MakenRosa <maken.cristhian@gmail.com>
2023-04-16 16:13:50 -0300

    Fix settings

--- projeto-todo/todo/todo/settings.py
+++ projeto-todo/todo/todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--_!1d0#&oau$ak&s_(b^%-sa#$$l3xx@u!9*_!=%%i+ez(7%b='
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
